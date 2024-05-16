170e1ea9ba71e42d4436e18339b97327e9a71a39
Mohau Mushi <david1mushi1@gmail.com>
2024-05-16 15:37:18 +0200

    Create a function which is responsible forhandling for loading more books and createdan event listener and added it into setupEventListener which is for when the user clicks the button to function correctly

--- scripts.js
+++ scripts.js
@@ -129,6 +129,7 @@ function setupEventListeners() {
     });
 
     data.search.form.addEventListener('submit', handlingSearchSubmit);
+    data.list.button.addEventListener('click', handleShowMore);
 }
 
 function handlingSearchSubmit(event) {
@@ -173,32 +174,17 @@ function handlingSearchSubmit(event) {
     data.search.overlay.open = false;
 }
 
-data.list.button.addEventListener('click', () => {
-    const fragment = document.createDocumentFragment()
+function handleShowMore() {
+    const fragment = document.createDocumentFragment();
 
-    for (const { author, id, image, title } of matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE)) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-    
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-
-        fragment.appendChild(element)
+    for (const book of matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE)) {
+        fragment.appendChild(createBookPreview(book));
     }
 
-    data.list.items.appendChild(fragment)
-    page += 1
-})
+    data.list.items.appendChild(fragment);
+    page += 1;
+    
+}
 
 data.list.items.addEventListener('click', (event) => {
     const pathArray = Array.from(event.path || event.composedPath())
